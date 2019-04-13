298f20b25aaa781fd29dfbe164c7ba2ee569b1d7
Jaiveer <32986920+CallMeJai@users.noreply.github.com>
2019-04-13 14:06:09 -0700

    add PD controller, choose direction based off spread and error, and it works

--- line_follower/scripts/line_follow.py
+++ line_follower/scripts/line_follow.py
@@ -24,12 +24,16 @@ class LineFollower:
         self.down = Twist()
         self.stop = Twist()
 
-        self.right.linear.x = 0.125
-        self.left.linear.x = -0.125
+        self.right.linear.x = 0.088
+        self.left.linear.x = -0.088
         self.up.linear.z = -0.125
         self.down.linear.z = 0.125
 
         self.corrected_msg = Twist()
+        # for deciding orientation of line3
+        # rows are first index, columns are second
+        self.largestRowValue = 0
+        self.largestColValue = 0
         # axes of an image increase left to right and bottom to top
         self.y_directions = {1 : self.down, -1 : self.up}
         self.x_directions = {1 : self.right, -1 : self.left}
@@ -37,14 +41,16 @@ class LineFollower:
         self.direction_msg = Twist()
         # first value is the axis and the second is the direction
         self.prev_direction =['', '']
-        # threshold of error (in pixels) for changing main directions
-        self.threshold = 16
+
         self.y_err = 0
         self.x_err = 0
-        self.largest_err = 0
-        self.other_err = 0
+        self.err = 0
         self.err_axis = ''
-        self.other_axis = ''
+        # for PD (no integral)
+        self.prev_err = 0
+        self.prev_time = rospy.Time.now()
+        self.kP = 0.00225
+        self.kD = 0.0007
 
     def makeDirectionMessage(self, error, axis):
         try:
@@ -57,21 +63,43 @@ class LineFollower:
         except ZeroDivisionError:
             return self.axes.get(self.prev_direction[0], self.axes).get(self.prev_direction[1], self.stop)
 
-    # stopgap, should be some type of PID for best results
+    # funky PD controller
+    # (abs(error)/error)*(abs(error) ** 0.44)/35
     def correctError(self, error):
+        correction = 0
         try:
-            print("errcorr: ", (abs(error)/error)*abs(error)**0.36/30) 
-            return (abs(error)/error)*abs(error)**0.36/30
+            print("dcorr: ", self.kD * (error - self.prev_err) / (rospy.Time.now() - self.prev_time).to_sec())
+            print("derr: ", (error - self.prev_err))
+            correction = self.kP * error + self.kD * (error - self.prev_err) / (rospy.Time.now() - self.prev_time).to_sec()
         except ZeroDivisionError:
-            return 0
+            print("dcorr: NaN no time")
+            correction = self.kP * error
+        self.prev_time = rospy.Time.now()
+        self.prev_err = error
+        print("pcorr: ", self.kP * error)
+        print("correction", correction)
+        print('time:', self.prev_time)
+        return correction
+
+    def calcSpread(self, img):
+        height, width = img.shape
+        colSums = [0] * width
+        rowSums = []
+        for row in img:
+            rowSums.append(sum(row))
+        for column in range(height):
+            for row in img:
+                colSums[column] += row[column]
+        self.largestRowValue = max(rowSums)
+        self.largestColValue = max(colSums)
 
     def keepCentered(self, error, axis):
-        print(axis)
-        print(error)
+        print("correcting error on", axis)
+        print("error", error)
         self.corrected_msg = self.direction_msg
-        if axis is 'y':
+        if axis is 'x':
             self.corrected_msg.linear.x = self.correctError(error)
-        elif axis is 'x':
+        elif axis is 'y':
             self.corrected_msg.linear.z = self.correctError(error)
         return self.corrected_msg
 
@@ -85,7 +113,7 @@ class LineFollower:
         # downsampling to 212 x 160 to speed things up
         img_height, img_width, channels = image.shape
         height = 160
-        width = 160 
+        width = 160
         newsize = (width, height)
         interpolation = cv2.INTER_NEAREST
         # cropping image to one half because sim cam has something blocking it
@@ -95,17 +123,17 @@ class LineFollower:
         # convert to hsv for better color thresholding
         hsv = cv2.cvtColor(resized_img, cv2.COLOR_BGR2HSV)
 
-        # filtering image to red and not red since only those colors matter
-        # since our hue values wrap from 330 to 30, but that's not how being
-        # in between numbers works, we gotta make two masks and combine them
-        lower_red = np.array([330, 60, 60])
-        inbw_red1 = np.array([0, 255, 255])
+        # masking image to red and not red since only those colors matter
+        # red goes around 360 (about 330 to 30) but cv sucks so its hues (and vals and saturations)go 0 to 255
+        # so we'll just say red starts at 160 and ends at 20
+        # https://docs.opencv.org/3.4/de/d25/imgproc_color_conversions.html
+        lower_red = np.array([160, 60, 60])
+        inbw_red1 = np.array([255, 255, 255])
         inbw_red2 = np.array([0, 60, 60])
-        upper_red = np.array([30, 255, 255])
+        upper_red = np.array([20, 255, 255])
         mask1 = cv2.inRange(hsv, lower_red, inbw_red1)
         mask2 = cv2.inRange(hsv, inbw_red2, upper_red)
         mask = mask1 + mask2
-        print(mask)
         # find centroid which will be used to keep line-following on track
         # and decide when to change direction and what direction to change to
         m = cv2.moments(mask, False)
@@ -122,45 +150,54 @@ class LineFollower:
         cv2.circle(centroid,(int(cx), int(cy)), 10,(0,255,0),-1)
         cv2.imshow("centroid", centroid)
         cv2.waitKey(1)
-        # choosing direction
         self.x_err = cx - (width/2)
         self.y_err = cy - (height/2)
 
-        # TODO sanity check this
+        # update axis of direction
+        self.calcSpread(mask)
+
+        # TODO sanity check this direction choosing
         if self.prev_direction[0] is not 'x' and self.prev_direction[0] is not 'y':
-            if abs(self.x_err) > abs(self.y_err):
+            if self.largestColValue > self.largestRowValue:
                 # gotta start with least amount of line in frame
-                self.largest_err = self.x_err
-                self.other_err = self.y_err
+                print('y choose')
+                self.direction_msg = self.makeDirectionMessage(self.y_err, 'y')
+                # error to correct
+                self.err = self.x_err
                 self.err_axis = 'x'
-                self.other_axis = 'y'
-                print('x choose')
-                self.direction_msg = self.makeDirectionMessage(self.largest_err, self.err_axis)
             else:
                 # gotta start with least amount of line in frame
-                self.largest_err = self.y_err
-                self.other_err = self.x_err
+                print('x choose')
+                self.direction_msg = self.makeDirectionMessage(self.x_err, 'x')
+                # error to correct
+                self.err = self.y_err
                 self.err_axis = 'y'
-                self.other_axis = 'x'
-                print('y choose')
-                self.direction_msg = self.makeDirectionMessage(self.largest_err, self.err_axis)
         elif self.prev_direction[0] is 'x':
-            self.largest_err = self.x_err
-            self.other_err = self.y_err
-            print('x continue')
+            # error to correct
+            self.err = self.y_err
+            self.err_axis = 'y'
+            # changing direction
+            if self.largestColValue > self.largestRowValue:
+                # gotta start with least amount of line in frame
+                print('y choose in x')
+                self.direction_msg = self.makeDirectionMessage(self.y_err, 'y')
+                # error to correct
+                self.err = self.x_err
+                self.err_axis = 'x'
         elif self.prev_direction[0] is 'y':
-            self.largest_err = self.y_err
-            self.other_err = self.x_err
-            print('y continue')
-        if not (-self.threshold < self.other_err < self.threshold):
-            # swapping directions
-            self.largest_err, self.other_err = self.other_err, self.largest_err
-            self.err_axis, self.other_axis = self.other_axis, self.err_axis
-            print('thresh swap')
-            # general movement direction
-            self.direction_msg = self.makeDirectionMessage(self.largest_err, self.err_axis)
+            # error to correct
+            self.err = self.x_err
+            self.err_axis = 'x'
+            # changing direction
+            if self.largestRowValue > self.largestColValue:
+                # gotta start with least amount of line in frame
+                print('x choose in y')
+                self.direction_msg = self.makeDirectionMessage(self.x_err, 'x')
+                # error to correct
+                self.err = self.y_err
+                self.err_axis = 'y'
         # error correction
-        self.movement_pub.publish(self.keepCentered(self.other_err, self.err_axis))
+        self.movement_pub.publish(self.keepCentered(self.err, self.err_axis))
 
 def main():
     rospy.init_node('line_follower')
